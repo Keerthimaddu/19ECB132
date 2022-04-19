136d29e4f7c77b243a0d2ecb365775d3392cddfd
Keerthimaddu <101540034+Keerthimaddu@users.noreply.github.com>
2022-04-19 21:26:27 +0530

    Create deleting_arrray.c

---
+++ deleting_arrray.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main()
+{
+	int array[10],position,i,n;
+	printf("enter n");
+	scanf("%d",&n);
+	printf("enter %d",n);
+	for(i=0;i<n;i++)
+	scanf("%d",&array[i]);
+	printf("enter position");
+	scanf("%d",&position);
+	if(position>=n+1)
+	printf("deletion not possible");
+	else
+	{
+		for(i=position-1;i<n-1;i++)
+		array[i]=array[i+1];
+		printf("resultant array is");
+		for(i=0;i<n-1;i++)
+		printf("%d",array[i]);
+	}
+	
+}
