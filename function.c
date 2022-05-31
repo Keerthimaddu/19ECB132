12c90fde7a1e0fb36f4b510b3257f37eb3736a2a
Keerthimaddu <101540034+Keerthimaddu@users.noreply.github.com>
2022-05-31 19:55:34 +0530

    Update function.c

--- function.c
+++ function.c
@@ -4,7 +4,7 @@ main()
   int p,t,r,s;
   p=40,t=60,r=5;
   s=simple_interest(p,t,r);
-  print("%d",s);
+  printf("%d",s);
 }
 int simple_interest(int a, int b, int c)
 {
@@ -12,3 +12,10 @@ int simple_interest(int a, int b, int c)
   k=(a*b*c)/100;
   return(k);
 }
+
+
+/* Output
+
+120
+
+*/
