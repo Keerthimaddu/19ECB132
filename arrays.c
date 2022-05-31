535f0f42591281d3288383ac61b28af74b0e261e
Keerthimaddu <101540034+Keerthimaddu@users.noreply.github.com>
2022-05-31 20:00:10 +0530

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -4,6 +4,13 @@ main()
   int i; int arr[10]={1,2,3,4,5,6,7,8,9};
   {
     for(i=0;i<=9;i++)
-      printf("%d",arr[i]);
+      printf("%d ",arr[i]);
   }
 }
+
+
+/* Output
+
+1 2 3 4 5 6 7 8 9 0
+
+*/
